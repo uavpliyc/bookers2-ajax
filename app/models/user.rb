6caa5d37ab95f26942130adc1ab523b3b25ac5c0
EC2 Default User <ec2-user@ip-172-31-75-173.ec2.internal>
2021-03-03 11:04:18 +0000

    make_search_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,22 @@ class User < ApplicationRecord
   def following?(user)
     following_user.include?(user)
   end
+  
+  def User.search(search, user_or_book, how_search)
+    if user_or_book == "1"
+      if how_search == "1"
+              User.where(['name LIKE ?', "%#{search}%"])
+      elsif how_search == "2"
+              User.where(['name LIKE ?', "%#{search}"])
+      elsif how_search == "3"
+              User.where(['name LIKE ?', "#{search}%"])
+      elsif how_search == "4"
+              User.where(['name LIKE ?', "#{search}"])
+      else
+              User.all
+      end
+    end
+  end
 
   attachment :profile_image, destroy: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'search/search'
   devise_for :users
   
   resources :users, only: [:show, :index, :edit, :update] do
@@ -23,5 +22,6 @@ Rails.application.routes.draw do
 
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get "search" => "search#index"
 
 end
