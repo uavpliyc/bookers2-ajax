d0b4055d811328bd64c7279ab9fb9deb2ad914d3
EC2 Default User <ec2-user@ip-172-31-68-116.ec2.internal>
2021-03-04 10:39:56 +0000

    finish

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,8 @@ class FavoritesController < ApplicationController
   before_action :book_params
 
   def create
-    @favorite = current_user.favorites.create(book_id: params[:book_id])
+    @favorite = current_user.favorites.create(book_id: @book.id)
+    @book = Book.find(params[:book_id])
     # redirect_back(fallback_location: root_path)
   end
 
