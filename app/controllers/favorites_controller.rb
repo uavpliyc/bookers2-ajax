1eb538c291f6e0fd66ba2c47d3a12a2091bec053
EC2 Default User <ec2-user@ip-172-31-68-116.ec2.internal>
2021-03-04 07:16:54 +0000

    favorite_Ajax

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,21 @@
+class FavoritesController < ApplicationController
+  before_action :book_params
+
+  def create
+    @favorite = current_user.favorites.create(book_id: params[:book_id])
+    # redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
+    # redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def book_params
+    @book = Book.find(params[:book_id])
+  end
+
+end
